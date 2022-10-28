c3f604304395a3043bf9ccc1e819ad0ed36624d1
Innocent Abugu <innocentchukwuebuka90@gmail.com>
2022-10-28 10:48:31 -0700

    Delete week5/demo server directory

--- week5/demo server/demo/path-demo.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-let filePath = path.join(__dirname, 'path-demo.js');
-
-let fileExt = path.extname(filePath);
-
-let basename = path.basename(filePath);
-console.log(basename)
